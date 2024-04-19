60d3b10a233df0dd2a566f9d75b5d4984ee5a9bc
XinrZhou <1352492197@qq.com>
2024-04-19 23:16:55 +0800

    feat: Echarts图表数据配置

--- src/constant/chart.ts
+++ src/constant/chart.ts
@@ -18,7 +18,7 @@ export const headerCarts = [
       xAxis: {
         data: getCurrentDate(14),
         axisLabel: {
-          interval: 0, 
+          interval: 1, 
           rotate: 20 
         }
       },
@@ -32,7 +32,7 @@ export const headerCarts = [
       },
       series: [
         {
-          data: getRandomCount(14, 200, 1000),
+          data: getRandomCount(14, 600, 1000),
           type: 'line',
           smooth: true
         }
@@ -40,12 +40,60 @@ export const headerCarts = [
     }
   },
   {
-    title: '类目总成交额（近15日）',
+    title: '搜索趋势',
+    span: 8,
+    option: {
+      tooltip: {
+        trigger: 'item'
+      },
+      grid: {
+        left: 48,
+        top: 20,
+        right: 16,
+        bottom: 40
+      },
+      dataset: [
+        {
+          dimensions: ['name', 'count'],
+          source: [
+            ['谷雨茶', 936],
+            ['轻食', 540],
+            ['有机蔬菜', 594],
+            ['甜粽or咸粽', 821],
+            ['热带水果', 458],
+            ['冰淇淋', 668],
+            ['嫩笋', 798],
+            ['火锅食材', 385],
+            ['零糖零卡', 420],
+            ['初夏水果', 615],
+          ]
+        },
+        {
+          transform: {
+            type: 'sort',
+            config: { dimension: 'count', order: 'desc' }
+          }
+        }
+      ],
+      xAxis: {
+        type: 'category',
+        axisLabel: { interval: 0, rotate: 30 }
+      },
+      yAxis: {},
+      series: {
+        type: 'bar',
+        encode: { x: 'name', y: 'score' },
+        datasetIndex: 1
+      }
+    }
+  },
+  {
+    title: '近14日总成交额（GMV）',
     span: 8,
     option: {
       tooltip: {
         trigger: 'item',
-        formatter: '{b} : {c} ({d}%)'
+        formatter: '{b} : {c}元 ({d}%)'
       },
       series: [
         {
@@ -57,15 +105,15 @@ export const headerCarts = [
             borderRadius: 5
           },
           data: [
-            { value: 11, name: '果蔬鲜花' },
-            { value: 5, name: '海鲜水产' },
-            { value: 6, name: '粮油调味' },
-            { value: 6, name: '肉禽蛋品' },
-            { value: 6, name: '乳品烘焙' },
-            { value: 5, name: '休闲零食' },
-            { value: 7, name: '餐饮熟食' },
-            { value: 10, name: '酒水饮料' },
-            { value: 5, name: '预制菜面点' }
+            { value: 207910, name: '果蔬鲜花' },
+            { value: 86797.5, name: '粮油调味' },
+            { value: 216835.8, name: '海鲜水产' },
+            { value: 103307, name: '乳品烘焙' },
+            { value: 73289.6, name: '休闲零食' },
+            { value: 120476, name: '餐饮熟食' },
+            { value: 96794, name: '酒水饮料' },
+            { value: 159805.5, name: '肉禽蛋品' },
+            { value: 156944.4, name: '预制菜面点' }
           ]
         }
       ]
@@ -76,7 +124,7 @@ export const headerCarts = [
 export const middleCharts = [
   {
     title: '全国RDC数量分布',
-    span: 14,
+    span: 12,
     isMap: true,
     option:   {
       tooltip: {
@@ -97,9 +145,8 @@ export const middleCharts = [
       visualMap: {
         min: 0,
         max: 10,
-        // text: ['高', '低'],
         inRange: {
-          color: ['#fff', '#79bbff','#d9ecff', '#337ecc'] // 配置颜色范围
+          color: ['#fff','#1ba784', '#428675'] // 配置颜色范围
         },
       },
       geo: [
@@ -107,12 +154,19 @@ export const middleCharts = [
           type: 'map',
           map: 'china', 
           roam: true, // 开启地图缩放、平移漫游等功能
-          zoom: 1.3, // 地图缩放级别
+          zoom: 1.4, // 地图缩放级别
+          top: 90,
           label: {
             show: true, // 是否显示地图区域名称
             fontSize: 10, // 地图区域名称字体大小
             color: '#333' // 地图区域名称字体颜色
           },
+          emphasis: {
+            focus: 'self',
+            itemStyle: {
+              color: '#bacf65'
+            }
+          }
         },
       ],
       series: [
@@ -160,92 +214,84 @@ export const middleCharts = [
     }
   },
   {
-    title: '类目库',
-    span: 10,
+    title: '近14日类目成交额',
+    span: 12,
     option: {
       tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross',
-          crossStyle: {
-            color: '#999'
-          }
-        }
-      },
-      toolbox: {
-        feature: {
-          dataView: { show: true, readOnly: false },
-          magicType: { show: true, type: ['line', 'bar'] },
-          restore: { show: true },
-          saveAsImage: { show: true }
-        }
+        trigger: 'item',
+        formatter: '{b} : {c}元'
       },
       legend: {
-        data: ['商品数', '成交额']
+        selectedMode: false
       },
-      xAxis: [
-        {
-          type: 'category',
-          data: [
-            '果蔬鲜花', '海鲜水产','粮油调味', '肉禽蛋品', '乳品烘焙', '休闲零食', '餐饮熟食', '酒水饮料', '预制菜面点'
-          ],
-          axisPointer: {
-            type: 'shadow'
-          },
-          axisLabel: {
-            interval: 0, 
-            rotate: 20 
-          }
-        }
-      ],
-      yAxis: [
-        {
-          type: 'value',
-          name: '商品数',
-          min: 0,
-          max: 200,
-          interval: 40,
-          axisLabel: {
-            formatter: '{value}'
-          }
-        },
-        {
-          type: 'value',
-          name: '成交额',
-          min: 0,
-          max: 10000,
-          interval: 500,
-          axisLabel: {
-            formatter: '{value}'
-          }
-        }
-      ],
+      xAxis: {
+        data: getCurrentDate(14)
+      },
+      yAxis: {},
       series: [
         {
-          name: '商品数',
+          data: [11050, 13650, 10850, 15850, 13350, 15359, 16350, 15896.5, 12850, 16850, 12850, 10850, 11850, 13850],
           type: 'bar',
-          tooltip: {
-            valueFormatter: function (value) {
-              return value + '件';
-            }
-          },
-          data: [
-            188, 164, 120, 136, 144, 65, 109, 43, 57
-          ]
+          stack: 'total',
+          barwidth: '60%',
+          name: '果蔬鲜花'
         },
         {
-          name: '成交额',
-          type: 'line',
-          yAxisIndex: 1,
-          tooltip: {
-            valueFormatter: function (value) {
-              return value + '元';
-            }
-          },
-          data: [
-            9736.8, 8200, 5500.3, 9765.4, 3784.2, 2400, 5932.7, 4253, 4892.1
-          ]
-        }
+          data: [6289.5, 5567, 5455, 7680, 7923, 5045, 6167, 5289.5, 5411, 5533.5, 8655.5, 4778, 4801, 6735.6],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '粮油调味'
+        },
+        {
+          data: [12488, 13488, 14488, 16488, 14988, 13988, 16988, 12988, 14988, 16488, 17488, 13488, 11488, 10488, 14488],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '海鲜水产'
+        },
+        {
+          data: [7292, 7961, 6293, 8628.6, 7965, 5962, 6960, 7630, 9294, 7959, 5626, 6295, 5237, 6297],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '乳品烘焙'
+        },
+        {
+          data: [7518.6, 7686.6, 4796, 5241.3, 6964, 5132, 6854.7, 6577.4, 4300.1, 4022.8, 5745.5, 5468.2, 3073.5, 4960],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '休闲零食'
+        },
+        {
+          data: [8458, 6643, 7230, 6475, 9367, 9542, 7750, 9126, 8071, 6814, 10505, 7038, 9380, 7059],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '餐饮熟食'
+        },
+        {
+          data: [7033, 8725.6, 7504, 8697.5, 6330, 6134, 7143, 5676.5, 8069, 7643.2, 9440, 6504.5, 7147, 1063.5],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '酒水饮料'
+        },
+        {
+          data: [8414, 9414, 10414, 12414, 13414, 14414, 10514, 12514, 13514, 12414, 10414, 9414, 8414, 11414],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '肉禽蛋品'
+        },
+        {
+          data: [9210, 10210, 12210, 10710, 12710, 11710, 10310, 11310, 12310, 13310, 12210, 11210, 9210, 8210],
+          type: 'bar',
+          stack: 'total',
+          barwidth: '60%',
+          name: '预制菜面点'
+        },
       ]
     }
   },
